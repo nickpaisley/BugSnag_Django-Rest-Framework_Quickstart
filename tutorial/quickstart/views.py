3d0b4b27cfcff5802596e191116e73e8be1913dd
Luke Belton <58511679+luke-belton@users.noreply.github.com>
2023-06-08 08:54:51 +0100

    notify with Error objects

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -9,22 +9,19 @@ from rest_framework.response import Response
 
 import bugsnag
 
-BUGSNAG = {
-    'api_key': '6dbeb063f84ebfbe0ad431672267e4db',
-    'project_root': '/',
-}
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=["post"])
     def notify_bugsnag(self, request):
-        bugsnag.notify("Button clicked")
+        bugsnag.notify(TypeError("Button clicked"))
         return Response("Bugsnag notified")
 
 
@@ -32,12 +29,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=["get"])
     def notify_bugsnag(self, request):
-        bugsnag.notify("Button clicked")
+        bugsnag.notify(TypeError("Button clicked"))
         return Response("Bugsnag notified")
-
